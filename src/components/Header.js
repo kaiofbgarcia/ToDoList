50d41f2d351cf179d559b33ba3b813a87b14830a
Kaio Garcia <kaiofbgarcia@gmail.com>
2022-10-30 20:03:26 -0300

    header att

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import {Rocket} from 'phosphor-react';
 function Header() {
   return (
 		<div className="header">
+			<Rocket size={32} className="rocket-icon"/>
 			<h1>Lista de Tarefas</h1>
 			<Rocket size={32} className="rocket-icon"/>
 		</div>
